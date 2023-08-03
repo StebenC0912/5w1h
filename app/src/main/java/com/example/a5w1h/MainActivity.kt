40e9e4afba38ebc361cc7392a30f4a1c7f468ebc
stebenc <stebenc09122002@gmail.com>
2023-08-03 09:22:54 +0700

    update ViewBinding in MainActivity.kt

--- app/src/main/java/com/example/a5w1h/MainActivity.kt
+++ app/src/main/java/com/example/a5w1h/MainActivity.kt
@@ -3,15 +3,15 @@ package com.example.a5w1h
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
+import com.example.a5w1h.databinding.ActivityMainBinding
 import com.example.a5w1h.fragment.MainFragment
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var fragmentManager: FragmentManager
+    private lateinit var viewBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
     }
 }
